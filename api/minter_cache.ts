d3acaa7e1c510841103c63c6b350576a88b6cbc1
FoxeiZ <57582539+FoxeiZ@users.noreply.github.com>
2025-10-10 00:54:03 +0700

    feat: switch to Upstash Redis client and update cache data handling

--- api/minter_cache.ts
+++ api/minter_cache.ts
@@ -1,17 +1,19 @@
 import type { VercelRequest, VercelResponse } from '@vercel/node'
-import { createClient } from 'redis';
+import { Redis } from "@upstash/redis"
 import { SessionManager, YoutubeSessionDataCaches } from "../src/session_manager.js";
 
 
-const redisUrl = process.env.REDIS_URL;
-const redis = await createClient({ url: redisUrl }).connect();
+const redis = new Redis({
+    url: process.env.KV_REST_API_URL,
+    token: process.env.KV_REST_API_TOKEN,
+});
 
 export default async function handler(req: VercelRequest, res: VercelResponse) {
     const cache: YoutubeSessionDataCaches = {};
 
-    const cacheData = await redis.get('youtube_session_data');
+    const cacheData = await redis.get<object>('youtube_session_data');
     if (cacheData) {
-        const parsedData = JSON.parse(cacheData) as YoutubeSessionDataCaches;
+        const parsedData = cacheData as YoutubeSessionDataCaches;
         for (const contentBinding in parsedData) {
             const parsedCache = parsedData[contentBinding];
             if (parsedCache) {

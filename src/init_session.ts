55f40fe256dacf696c45f012f0cb205d9c60ab05
FoxeiZ <57582539+FoxeiZ@users.noreply.github.com>
2026-05-09 17:55:08 +0700

    fix: wrong import

--- src/init_session.ts
+++ src/init_session.ts
@@ -1,5 +1,5 @@
 import { Redis } from "@upstash/redis"
-import { SessionManager, YoutubeSessionDataCaches } from "./session_manager";
+import { SessionManager, YoutubeSessionDataCaches } from "./session_manager.ts";
 
 export default async function initSession() {
     const redis = new Redis({
